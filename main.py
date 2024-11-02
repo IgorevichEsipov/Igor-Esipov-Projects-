588683786c42f4bcf60dfd3b97aae629f2209b22
Igor I Esipov II <87508625+IgorevichEsipov@users.noreply.github.com>
2024-11-01 20:02:24 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,100 @@
-print('Welcome to am I a Mechanic test')
-answer = input('Are you ready to play the Quiz ?')
+import sys
+import pygame
+import random
+
+pygame.init()
+screen = pygame.display.set_mode((350,600))
+clock = pygame.time.Clock()
+
+class Apple:
+    def __init__(self, image, position, speed):
+        self.image = image
+        self.rect = self.image.get_rect(topleft = position)
+        self.speed = speed
+    def move(self):
+        self.rect.y += self.speed    
+
+# variables 
+
+speed = 3
 score = 0
-total_questions = 3
 
-if answer.lower() == 'yes':
-    answer = input('Question 1: What is an intake manifold? a) Part of the Induction System b) Part of the Cooling '
-                   'System c) Part of the Brake System')
-    if answer.lower() == 'a':
-        score += 1
-        print('Correct')
-    else:
-        print('Wrong Answer')
+# constants
+TILESIZE = 32
 
-    answer = input('Question 2: What is DOHC stand for? a) Dual Overhead Cam b) Dual Orifice Heat Collector c) Dual '
-                   'Oxygen Hydrogen Collector ')
-    if answer.lower() == 'a':
-        score += 1
-        print('Correct')
-    else:
-        print('Wrong Answer')
+# floor
+floor_image = pygame.image.load('assets/floor.png').convert_alpha()
+floor_image = pygame.transform.scale(floor_image, (TILESIZE*15,TILESIZE*5))
+floor_rect = floor_image.get_rect(bottomleft = (0, screen.get_height()))
 
-    answer = input('Question 3: What is part of the ignition system a) BCM b) Coil Spring c) Spark Plug ')
-    if answer.lower() == 'c':
-        score += 1
-        print('Correct')
-    else:
-        print('Wrong Answer')
+# player
+player_image = pygame.image.load('assets/player_static.png').convert_alpha()
+player_image = pygame.transform.scale(player_image, (TILESIZE, TILESIZE*2))
+player_rect = player_image.get_rect(center = (screen.get_width()/2, screen. get_height()-floor_image.get_height()-(player_image.get_height()/2)))
 
-print('Thank you for playing this quiz,', score, "questions correctly!")
-mark: float = (score / total_questions) * 100
-print('Marks obtained:', mark)
-print('Bye! ')
+# apple 
+apple_image = pygame.image.load('assets/apple.png').convert_alpha()
+apple_image = pygame.transform.scale(apple_image, (TILESIZE, TILESIZE))
+
+apples = [
+    Apple(apple_image, (100,0) , 3),
+    Apple(apple_image, (300,0) , 3),
+]
+
+# fonts 
+font = pygame.font.Font('assets/PixeloidMono.ttf', TILESIZE//2)
+
+# sound fx
+pickup = pygame.mixer.Sound('assets/powerup.mp3')
+pickup.set_volume(0.1)
+
+running = True
+
+
+def update():
+    global speed
+    global score
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        player_rect.x -= 8
+    if keys[pygame.K_RIGHT]:
+        player_rect.x += 8
+
+    #apple management
+    for apple in apples:
+        apple.move()
+        if apple.rect.colliderect(floor_rect):
+            apples.remove(apple)    
+            apples.append(Apple(apple_image, (random.randint(50,300), -50) , speed))
+        elif apple.rect.colliderect(player_rect):
+            apples.remove(apple)    
+            apples.append(Apple(apple_image, (random.randint(50,300), -50) , speed))
+            speed += 0.1
+            score += 1
+            pickup.play()
+
+def draw():
+    screen.fill('lightblue')
+    screen.blit(floor_image, floor_rect)
+    screen.blit(player_image, player_rect)
+
+    for apple in apples:
+        screen.blit(apple.image, apple.rect)
+    
+    score_text = font.render(f'Score: {score}', True, "white")
+    screen.blit(score_text, (5,5))
+# game loop
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    update()
+    draw()
+            
+    clock.tick(60)
+    pygame.display.update()
+        
